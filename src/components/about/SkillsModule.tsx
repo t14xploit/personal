776a69e5b601723fec4100211d7913c2af6d6368
T14Xploit <khrystyna.tyvoniuk@gmail.com>
2025-05-02 14:12:06 +0200

    skills-module style fixed

--- src/components/about/SkillsModule.tsx
+++ src/components/about/SkillsModule.tsx
@@ -7,12 +7,12 @@ const skills = {
   
   export default function SkillsModule() {
     return (
-      <section className="mt-10 px-6 py-8 bg-[#0b0f1c] border border-[#00f0ff22] rounded-lg">
+      <section className="mt-10 px-6 py-8  ">
         <h2 className="text-[#00f0ff] text-lg font-mono mb-6">[SKILLS MODULE]</h2>
         <div className="grid md:grid-cols-3 gap-6 text-[#9eeaf9] text-sm font-light">
           {Object.entries(skills).map(([category, list]) => (
             <div key={category}>
-              <h3 className="text-[#00ffff] font-semibold text-base mb-2">{category}</h3>
+              <h3 className="text-[#00ffff] font-light text-base mb-2">{category}</h3>
               <ul className="space-y-1 border-l-2 border-cyan-400/50 pl-4">
                 {list.map((item) => (
                   <li key={item}>→ {item}</li>
