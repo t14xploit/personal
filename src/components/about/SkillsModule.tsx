272e3f6ef180cca499785cb062ef20badc513404
T14Xploit <khrystyna.tyvoniuk@gmail.com>
2025-05-02 14:13:09 +0200

    color-fixes

--- src/components/about/SkillsModule.tsx
+++ src/components/about/SkillsModule.tsx
@@ -8,8 +8,8 @@ const skills = {
   export default function SkillsModule() {
     return (
       <section className="mt-10 px-6 py-8  ">
-        <h2 className="text-[#00f0ff] text-lg font-mono mb-6">[SKILLS MODULE]</h2>
-        <div className="grid md:grid-cols-3 gap-6 text-[#9eeaf9] text-sm font-light">
+        <h2 className=" text-lg font-mono mb-6">[SKILLS MODULE]</h2>
+        <div className="grid md:grid-cols-3 gap-6  text-sm font-light">
           {Object.entries(skills).map(([category, list]) => (
             <div key={category}>
               <h3 className="text-[#00ffff] font-light text-base mb-2">{category}</h3>
