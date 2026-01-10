e1cd0592c1c11ea35ac5bd55c912d096b4852b06
T14X <khrystyna.tyvoniuk@gmail.com>
2026-01-10 22:40:48 +0100

    test system notes, fixed global css

--- src/components/about/SystemNotes.tsx
+++ src/components/about/SystemNotes.tsx
@@ -4,14 +4,17 @@ export default function SystemNotes() {
       <div>
         <hr />
 <h2 className="font-light mt-6">{`> USER TRACE: Detailed Background Initialized`}</h2>
-<div className="mt-10 px-3 py-6  border border-[#00f0ff33] rounded-lg text-[#9eeaf9] font-light text-sm">
-        <h2 className="text-[#00f0ff] text-lg font-mono mb-3">[SYSTEM NOTES]</h2>
-        <ul className="space-y-2">
-          <li>🧠 Debugs with precision.</li>
-          <li>☕ Caffeinates like it’s a deployment pipeline.</li>
-          <li>🎯 Lives at the intersection of clean UI and secure backends.</li>
+<div className="mt-10 px-3 py-6  border border-[#00ff1e33] rounded-lg font-light text-sm">
+         <h2 className="text-[var(--accent)] text-lg font-mono mb-3">[SYSTEM NOTES]</h2>
+
+        <ul className="space-y-1 pl-4 list-inside text-[var(--foreground-muted)]">
+          <li>🧠 Analyzes logs precisely.</li>
+          <li>☕ Converts caffeine → code.</li>
+          <li>🔒 Security-first, always.</li>
+          <li>🎯 Optimizes UI & backend.</li>
         </ul>
-        <div className="mt-4 text-green-400 font-mono text-xs">
+
+        <div className="mt-3 text-[var(--green-accent)] font-mono text-xs">
           STATUS: <span className="animate-pulse">ONLINE</span>
         </div>
       </div>
